4f6562f5241b470f19e64b88c3a470f6514bb1d1
Yurij Storchak <kosnicer@mail.ru>
2014-01-14 16:44:04 +0200

    This version work. Almost

--- FirstProject/src/main/java/com/mozidev/firstproject/MainActivity.java
+++ FirstProject/src/main/java/com/mozidev/firstproject/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity  extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-
         switch(v.getId()) {
             case R.id.btnTop:
                 startActivity(new Intent(this, TwoActivity.class));
