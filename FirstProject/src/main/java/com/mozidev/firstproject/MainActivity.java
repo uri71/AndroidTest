2a17f831efbbefdd596f95fa568407af1b59d86f
Yurij Storchak <kosnicer@mail.ru>
2014-01-15 15:10:41 +0200

    final version

--- FirstProject/src/main/java/com/mozidev/firstproject/MainActivity.java
+++ FirstProject/src/main/java/com/mozidev/firstproject/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 break;
         }
 
-        Intent intent = new Intent(MainActivity.this, target);
+        Intent intent = new Intent(this, target);
         startActivity(intent);
     }
 }
