4b1e0405049cb88716a06151e851c3e485c0761b
Yurij Storchak <kosnicer@mail.ru>
2014-02-26 09:36:31 +0200

    IN-10
    Version 15

--- FirstProject/src/main/java/com/mozidev/firstproject/MyTabActivity.java
+++ FirstProject/src/main/java/com/mozidev/firstproject/MyTabActivity.java
@@ -1,61 +1,69 @@
 package com.mozidev.firstproject;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TabHost;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 
-public class MyTabActivity extends FragmentActivity {
+public class MyTabActivity extends ActionBarActivity implements ActionBar.TabListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_tab);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new MyTabFragment())
-                    .commit();
-        }
+        ActionBar mActionBar = getSupportActionBar();
+        mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        ActionBar.Tab tabDiscover1 = mActionBar.newTab().setText("Countries 1").setTabListener(this);
+        mActionBar.addTab(tabDiscover1);
+
+        ActionBar.Tab tabDiscover2 = mActionBar.newTab().setText("Countries 2").setTabListener(this);
+        mActionBar.addTab(tabDiscover2);
     }
 
+//        TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+//        tabHost.setup();
+//
+//        TabHost.TabSpec tabSpec1 = tabHost.newTabSpec("1");
+//        tabSpec1.setIndicator("List of the Countries 1");
+//        tabSpec1.setContent(new Intent(this, CountriesActivity.class));
+//        tabHost.addTab(tabSpec1);
+//
+//        TabHost.TabSpec tabSpec2 = tabHost.newTabSpec("2");
+//        tabSpec2.setIndicator("List of the Countries 2");
+//        tabSpec2.setContent(new Intent(this, LocalesActivity.class));
+//        tabHost.addTab(tabSpec2);
+//
+//        tabHost.setCurrentTabByTag("1");
 
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        Fragment fragment;
+        int position = tab.getPosition();
+        switch (position) {
+            case 0:
+                fragment = new CountriesFragment();
+                break;
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class MyTabFragment extends Fragment {
-
-        public MyTabFragment() {
+            default:
+                fragment = new SecondFragment();
+                break;
         }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_my_tab, container, false);
-
-            TabHost tabHost;
-            tabHost = (TabHost) rootView.findViewById(android.R.id.tabhost);
-            tabHost.setup();
-
-            TabHost.TabSpec tabSpec = tabHost.newTabSpec("1");
-            tabSpec.setIndicator("List of the Countries 1");
-            tabSpec.setContent(new Intent(getActivity(),CountriesActivity.class));
-            tabHost.addTab(tabSpec);
-
-            tabSpec = tabHost.newTabSpec("2");
-            tabSpec.setIndicator("List of the Countries 2");
-            tabSpec.setContent(new Intent(getActivity(),LocalesActivity.class));
-            tabHost.addTab(tabSpec);
-
-            tabHost.setCurrentTabByTag("1");
-
-            return rootView;
-        }
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
     }
 
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
 }
